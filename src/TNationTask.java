bfc2cf6196b8dee24613871b9846d68984133e80
Davor <davor.zigic@gmail.com>
2017-05-18 12:19:57 +0200

    initial commit

---
+++ src/TNationTask.java
@@ -0,0 +1,81 @@
+
+
+public class TNationTask {
+
+
+	/***
+	 * 
+	 * @param s string inputed
+	 * @param maxChar num of chars that we want to split string
+	 * @return returns array of splitted strings
+	 */
+	public static String[] maxNumOfChar(String s, int maxChar, int maxLines) {
+
+		int arrayLength = s.length() / maxChar;
+
+		String[] substrings = new String[maxLines];
+
+		if (s.length() == 0) {
+			System.out.println("String is empty.");
+			return null;
+		} else if (s.length() < maxChar) {
+			System.out.println("You have to enter less max number of chars");
+		} else {
+
+			int j = 0;
+			int lastIndex = substrings.length - 1;
+
+			for (int i = 0; i < maxLines-1; i++) {
+
+				if(i == 0) {
+					substrings[i] = s.substring(j, j + maxChar);
+					j += maxChar;
+				} else {
+				// if the first char in line is space
+					String test = s.substring(j-1, j);
+					if(test != " ") {
+						substrings[i] = " " + s.substring(j-1, j + maxChar);
+						j += maxChar;
+					} else {
+						substrings[i] = s.substring(j, j + maxChar);
+						j += maxChar;
+					}
+				
+				}
+				
+			}
+			String empty = s.substring(j-1,j);
+			if(empty != " ") {
+				substrings[maxLines-1] = " " + s.substring(j-1);
+			} else {
+				substrings[maxLines-1] = s.substring(j-1);
+			}
+			
+
+			return substrings;
+		}
+		
+		return null;
+	}
+	
+	public static void maxLines(int maxLines) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String test = "9:00 John Doe until 10 30 at Office";
+		
+		String[] string = maxNumOfChar(test, 11, 4);
+		
+
+		for (int i = 0; i < string.length; i++) {
+			System.out.println(string[i]);
+		}
+		
+		
+
+	}
+
+}
---
+++ src/TestingClass.java
@@ -0,0 +1,13 @@
+
+public class TestingClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String something = "hello there";
+		
+		System.out.println(something.substring(3, 5));
+
+	}
+
+}
